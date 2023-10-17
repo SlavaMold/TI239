27078afb28fff556c056977e434795e692f64639
SlavaMold <108458488+SlavaMold@users.noreply.github.com>
2023-10-17 18:02:45 +0300

    Add files via upload

---
+++ pages/test.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<style> 
+
+.article2 for + for, .article2:hover for { display: none; }
+.article2 for, .article2:hover for + for { display: block; }
+	 </style>
+	</head>
+<body>
+	<a href="/faq" title="FAQ">
+  <div class="article2">
+    <img src="/images/logo.png" alt="текст">
+    <for>ABC</for><for>DEF</for>
+  </div>
+</a>
+</body>
+</html>
